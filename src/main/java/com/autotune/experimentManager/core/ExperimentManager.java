be59d5312cad0d02a7b9093e4b5d4711cfd3bcd3
bharathappali <abharath@redhat.com>
2022-06-16 14:05:29 +0530

    Saving work
    Signed-off-by: bharathappali <abharath@redhat.com>

--- src/main/java/com/autotune/experimentManager/core/ExperimentManager.java
+++ src/main/java/com/autotune/experimentManager/core/ExperimentManager.java
@@ -3,7 +3,7 @@ package com.autotune.experimentManager.core;
 import com.autotune.experimentManager.services.ListTrialStatus;
 
 import com.autotune.experimentManager.services.CreateExperimentTrial;
-import com.autotune.experimentManager.services.ListExperimentTrail;
+import com.autotune.experimentManager.services.ListExperimentTrial;
 import com.autotune.experimentManager.settings.EMS;
 import com.autotune.experimentManager.utils.EMConstants;
 
@@ -70,7 +70,7 @@ public class ExperimentManager {
     private static void addEMServlets(ServletContextHandler context) {
         LOGGER.info(EMConstants.Logs.ExperimentManager.ADD_EM_SERVLETS);
         context.addServlet(CreateExperimentTrial.class, ServerContext.EXPERIMENT_MANAGER_CREATE_TRIAL);
-        context.addServlet(ListExperimentTrail.class, ServerContext.EXPERIMENT_MANAGER_LIST_EXPERIMENT_TRIAL);
+        context.addServlet(ListExperimentTrial.class, ServerContext.EXPERIMENT_MANAGER_LIST_EXPERIMENT_TRIAL);
         context.addServlet(ListTrialStatus.class, ServerContext.EXPERIMENT_MANAGER_LIST_TRIAL_STATUS);
     }
 }
